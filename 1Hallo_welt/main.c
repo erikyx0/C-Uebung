a00b2db0b45c0fa72bb4c07fa9a2350a8819f6b2
Ben Weckend <ben.weckendeb.de>
2023-11-02 21:20:30 +0100

    save berfore work

--- 1Hallo_welt/main.c
+++ 1Hallo_welt/main.c
@@ -2,26 +2,35 @@
 #include <string.h>
 
 int main(){
-    int x=0, y=0;
     char bname[] = "root";
-    char bnametry[] = "";
+    char bnametry[100];
     char pname[] = "tool";
-    char pnametry[] = "";
-
+    char pnametry[100];
+    int result, result2;
     do {
+        
+        int x=0;
         printf("Username: "); scanf("%s", &bnametry);
         while(getchar() != '\n');
         printf("Passwort: "); scanf("%s", &pnametry);
         while(getchar() != '\n');
 
-        if (strcmp(pname, pnametry) == 1){
+        result = strcmp(pname, pnametry);
+        result2 = strcmp(bname, bnametry);
+
+
+        if (result2 == 1){
             printf("Wrong password\n");
+            x++;
         }
-        if (strcmp(bname, bnametry) == 1){
+        if (result  == 1){
             printf("Wrong username\n");
+            x++;
         }
+        if (x!=0) printf("Try again!\n"); 
+        printf("%d", x);
         
-    }while(strcmp(bname, bnametry) == 0 && strcmp(pname, pnametry) == 0);
+    }while(result2 == 1 || result == 1);
 
     printf("Wellcome: %s\n", bnametry);
 
