69dcc4c721134112fbcb7d94c460ae09b057c853
Ben Weckend <ben.weckendeb.de>
2023-11-03 22:11:52 +0100

    fertigdtellung vom sortieren in 5stdlib

--- 5stdlib/main.c
+++ 5stdlib/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 int save(int *Snumbers, int *size, int count);
 void view(int *Vnumbers, int *size);
-void order();
+void order(int *Onumbers, int *Osize);
 int main(){
     char choice;
     int *numbers; 
@@ -11,8 +11,8 @@ int main(){
     numbers = malloc(sizeof(int) * size);
 
     do {
-    printf("Please make a choice:\n - save data (type s)\n - view data (type v)\n - exit      (type e)\nType here:  ");
-    scanf("%c", &choice);
+    printf("Please make a choice:\n - save data (type s)\n - view data (type v)\n - sort data (type o)\n - exit      (type e)\nType here:  ");
+    scanf("%c", &choice); printf("\n");
     switch (choice)
     {
     case 'v':
@@ -24,7 +24,7 @@ int main(){
     case 'e': 
         break;
     default: 
-        printf("\nInvalid Syyntax!\n\n"); break;
+        printf("\nInvalid Syntax!\n\n"); break;
     }
 
     
@@ -46,27 +46,55 @@ int save(int *Snumbers, int *Ssize, int count){
         printf("Save number (-1 to cancel): ");
         scanf("%d", &num);
 
-        if (num != -1) Snumbers[count] = num; count++;
+        if (num != -1) {Snumbers[count] = num; count++;}
+        
+        
 
     }while(num != -1);
+
+    printf("count:%d and Ssize:%d\n", count, *Ssize);
+    while (count<*Ssize){
+        (*Ssize)--;
+    }
+    printf("NEW: count:%d and Ssize:%d\n", count, *Ssize);
     while(getchar() != '\n');
-    return count;
+
+    return (count);
 }
 
 void view(int *Vnumbers, int *Vsize){
     printf("size: %d\n", *Vsize);
     for (int i=0; i<*Vsize; i++){
-        if (i<*Vsize-1) printf("numbers[%d] = %d\n", i, Vnumbers[i]);
+        if (i<*Vsize) printf("numbers[%d] = %d\n", i, Vnumbers[i]);
     }
     while(getchar() != '\n');
 }
 
 void order(int *Onumbers, int *Osize){
-
-    for (int i = 0; i<*Osize; i++){
-        if (Osize+i<(Osize+i+1)){
-            printf("Number: ");
+    int orderCount = 0;
+    printf("Osize: %d, Onumbers[i]: %d\n", *Osize, Onumbers[1]);
+    while(orderCount < *Osize){
+        orderCount = 0;
+        printf("----------------\n");
+        for (int i = 0; i<*Osize; i++){
+            if (Onumbers[i]<Onumbers[i+1]){
+                printf("Bigger\n");
+                orderCount++;
+            }else if (Onumbers[i]==Onumbers[i+1]){
+                printf("Same\n");
+                orderCount++;
+            }else{
+                printf("Smaller\n");
+                int shipping=0;
+                shipping = Onumbers[i];
+                Onumbers[i] = Onumbers[i+1];
+                Onumbers[i+1] = shipping;
+            }
         }
+        
     }
-
+    for(int i=0; i<*Osize; i++){
+        printf("Number[%d] = %d\n",i, Onumbers[i]);
+    }
+    while(getchar() != '\n');
 }
