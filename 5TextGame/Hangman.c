e89761a0cbfff6b9ef3ce3aaa142d5006cd86727
Ben Weckend <ben.weckend@web.de>
2023-11-07 20:00:56 +0100

    Hangman Game bug fixing -> still needs care

--- 5TextGame/Hangman.c
+++ 5TextGame/Hangman.c
@@ -1,36 +1,58 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 int main(){
     char word[20]; char guess;
     int wordlength = 0;
-    int gamecounter = 0;
     int maxFail = 0;
     int correctguess = 0;
-    printf("Type the word: "); scanf("%s", word);
+    bool included = false, schongeraten = false;
+
+    int *geratendeBS = malloc(sizeof(char) * 1);
+
+    printf("Welches Wort: "); scanf("%s", word);
     for (int i=0; word[i] != 0; i++){wordlength++;printf("*");} printf("\n");
 
-    while (gamecounter<wordlength && maxFail<3)
+    while (correctguess<wordlength && maxFail<3)
     {
-        int wordlengthTry=0;
-        printf("Guess: "); while(getchar() != '\n'); scanf("%c", &guess);
-        printf("Test: %c, %d ", guess, wordlength);
 
-        for (int i=0; i<wordlengthTry; i++){
-            if (guess =! word[i]){
-                wordlengthTry++;
-                printf("%d", wordlengthTry);
+        int wordlengthTry=0;
+        printf("Char: "); while(getchar() != '\n'); scanf("%c", &guess);
+
+        for(int a=0; a < 10; a++){
+            if (guess == geratendeBS[a]){
+                printf("Schon geraten! raten etwas Anders!\n");
+                schongeraten = true;
+            }else {schongeraten = false;}
+        }
+
+        if (schongeraten == false){
+            for (int i=0; i<wordlength; i++){ 
+                if (guess == word[i]){
+                    included = true;
+                    break;
+                }else included = false;
+            }
+
+            if (included == true){
+                correctguess++;
+                printf("Richtig!\n");
+            }else {
+                if (maxFail>1){
+                    geratendeBS = realloc(geratendeBS, sizeof(char)*(maxFail+1));
+                }
+                geratendeBS[maxFail] = guess;
+                maxFail++;
+                printf("Falsch\n");
             }
         }
-        wordlength = wordlengthTry;
 
-        if (wordlengthTry == wordlength-1){
-            printf("The char %c is included\n", guess);
-            gamecounter++; correctguess++;
-        }else {
-            printf("The char %c is not included\n", guess);
-            maxFail++;
-        }
+        
     }
+    if (correctguess == wordlength) printf("Gewonnen :)\n");
+    else printf("Fail :(\n");
+
     return 0;
 }
