75162699ada82616466521893eaa2aa7b2da5db1
Ben Weckend <ben.weckend@web.de>
2023-11-10 17:31:02 +0100

    Hangman.c fertig

--- 5TextGame/Hangman.c
+++ 5TextGame/Hangman.c
@@ -4,19 +4,21 @@
 #include <stdlib.h>
 
 int main(){
-    char word[20]; char guess;
+    char word[20]; char guess; 
     int wordlength = 0, maxFail = 0, correctguess = 0;
     bool included = false, schongeraten = false;
-
-    int *geratendeBS = calloc(1 ,sizeof(char));
+    char *geratendeBS = calloc(1 ,sizeof(char));
 
     printf("Welches Wort: "); scanf("%s", word);
     for (int i=0; word[i] != 0; i++){wordlength++;printf("*");} printf("\n");
 
+    char *posWord = calloc(wordlength, sizeof(char));
+
+
     while (correctguess<wordlength && maxFail<3)
     {
         schongeraten = false;
-        printf("Char: "); while(getchar() != '\n'); scanf("%c", &guess);
+        printf("\nChar: "); while(getchar() != '\n'); scanf("%c", &guess);
 
         for(int a=0; a < 10; a++){
             if (guess == geratendeBS[a]){
@@ -31,32 +33,51 @@ int main(){
                     included = true;
                     break;
                 }else included = false;
+
+                
             }
 
             if (included == true){
                 if (correctguess>1){
-                    geratendeBS = realloc(geratendeBS, sizeof(char)*(maxFail+1));
+                    geratendeBS = realloc(geratendeBS, sizeof(char)*(correctguess+1));
                 }
+
                 geratendeBS[correctguess] = guess;
-                
+
                 for (int i=0; i <wordlength; i++){
                     if (guess == word[i]){
                         correctguess++;
                     }
                 }
-                printf("Richtig! Anzahl der korrekt geratende BS: %d\n", correctguess);
+
+                printf("Richtig!: ", correctguess);
+
             }else {
                 maxFail++;
-                printf("Falsch\n");
+                printf("Falsch!: ");
             }
-        }
 
-        
+            for(int i=0; i<wordlength; i++){
+                for(int j=0; j<wordlength; j++){
+                    if (geratendeBS[i]==word[j]){
+                       posWord[j] = geratendeBS[i];
+                    }
+                }
+            }
+
+            for (int i=0; i<wordlength; i++){
+                if (posWord[i] != 0){
+                    printf("%c", posWord[i]);
+                }else{
+                    printf("*");
+                }
+            }
+        }  
     }
 
-    if (correctguess == wordlength) printf("Gewonnen :)\n");
+    if (correctguess == wordlength) printf("\nGewonnen :)\n");
     else printf("\nFail :(\n");
 
-    free(geratendeBS); 
+    free(geratendeBS); free(posWord);
     return 0;
 }
