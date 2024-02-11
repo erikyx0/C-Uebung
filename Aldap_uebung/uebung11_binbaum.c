e445131d9e552ebe3d89eba9fa97bfe06033161b
Ben Weckend <ben.weckend@web.de>
2024-02-12 00:47:07 +0100

    tree

--- Aldap_uebung/uebung11_binbaum.c
+++ Aldap_uebung/uebung11_binbaum.c
@@ -1,7 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
+typedef struct blatt{
+    int data;
+    struct blatt *left, *right;
+}Blatt;
 
+Blatt *add_leaf(Blatt *head, int data);
+Blatt *free_tree(Blatt *head);
+void print_tree(Blatt *head);
+
+int main(void){
+    Blatt *tree;
+    for(int i=0; i<8; i++){
+        tree = add_leaf(tree, i);
+    }
+    tree = add_leaf(tree, -1);
+    tree = add_leaf(tree, 4);
+    tree = add_leaf(tree, 89);
+    print_tree(tree);
     return 0;
+}
+
+Blatt *add_leaf(Blatt *head, int data){
+    Blatt *newBlatt = calloc(1, sizeof(Blatt)), *Buffer, *trueHead;
+    newBlatt->data = data;
+    if(head == NULL){return newBlatt;}
+    else{
+        trueHead = head;
+        while(head != NULL){
+            Buffer = head;
+            if(data <= head->data) head = head->left;
+            else if(data > head->data) head = head->right;
+        }
+        if(data <= Buffer->data) Buffer->left = newBlatt;
+        else if(data > Buffer->data) Buffer->right = newBlatt;
+    }
+    return trueHead;
+}
+
+void print_tree(Blatt *head){
+/*
+    Blatt *left_max = head, *right_max = head;
+    int lmax=0, rmax=0, true_max;
+    while(left_max != NULL){
+        left_max = left_max->left;
+        lmax++;
+    }
+    while(right_max != NULL){
+        right_max = right_max->right;
+        rmax++;
+    }
+    if(lmax <= rmax) true_max = lmax;
+    else true_max = rmax;
+*/
+    if(head != NULL){
+        print_tree(head->left);
+        printf("%d ", head->data);
+        print_tree(head->right);
+    }
+}
+
+Blatt *free_tree(Blatt *head){
+    if(head != NULL){
+        free_tree(head->left);
+        free_tree(head->right);
+        free(head);
+    }
+    return NULL;
 }
